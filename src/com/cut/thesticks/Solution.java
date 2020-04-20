b4f1aab9841b2e58a396e42e468818d3a6cfa226
Md Rafiqul islam <rafiqul.senior.istvn@gmail.com>
2020-04-21 00:32:56 +0700

    cut the stick solution

---
+++ src/com/cut/thesticks/Solution.java
@@ -0,0 +1,64 @@
+package com.cut.thesticks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+	/*
+	 * Function for permutationEquation
+	 
+	 * @param int[] arr
+	 * 
+	 * @return int []
+	 * 
+	 * @author Md Rafiqul Islam
+	 */
+
+	// Complete the cutTheSticks function below.
+	static int[] cutTheSticks(int[] arr) {
+		List<Integer> list = new ArrayList<Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			list.add(arr[i]);
+		}
+		List<Integer> afterDeleted = new ArrayList<Integer>();
+		while (true) {
+			if (list.stream().distinct().count() == 1) {
+				afterDeleted.add(list.size());
+				break;
+
+			}
+			if (Arrays.stream(arr).distinct().count() == 1) {
+				afterDeleted.add(arr.length);
+				break;
+			}
+			
+			afterDeleted.add(list.size());
+			if (list.size() == 1)
+				break;
+			int min = list.stream().min(Comparator.comparing(Integer::valueOf)).get();
+			list = list.stream().filter(x -> x > min).collect(Collectors.toList());
+
+			for (int i = 0; i < list.size(); i++) {
+				list.set(i, list.get(i) - min);
+
+			}
+			list = list;
+
+		}
+
+		return afterDeleted.stream().mapToInt(i -> i).toArray();
+	}
+
+	public static void main(String[] args) {
+		int[] p = { 8, 8, 14, 10, 3, 5 ,14, 12 };
+		System.out.println(cutTheSticks(p));
+	}
+
+}
--- src/com/sequence/equation/Solution.java
+++ src/com/sequence/equation/Solution.java
@@ -12,32 +12,32 @@ public class Solution {
 	 * 
 	 * @param int k
 	 * 
-	 * @param int[] p 
+	 * @param int[] p
 	 * 
 	 * @return int []
 	 * 
 	 * @author Md Rafiqul Islam
 	 */
 
-    static int[] permutationEquation(int[] p) {    	
-    	Map<Integer, Integer> MapY=new HashMap<Integer, Integer>();
-    	int pVal=1;
-    	for (int i = 0; i < p.length; i++) {    
-    		//Given the values for  p1 ,p2 
-    		MapY.put(p[i],pVal);
-    	    pVal++;
+	static int[] permutationEquation(int[] p) {
+		Map<Integer, Integer> MapY = new HashMap<Integer, Integer>();
+		int pVal = 1;
+		for (int i = 0; i < p.length; i++) {
+			// Given the values for p1 ,p2
+			MapY.put(p[i], pVal);
+			pVal++;
 		}
-    	int index=0;
-    	for (int i = 1; i <= p.length; i++) {
-    		//p(p(y))=x
-    		p[index]=MapY.get(MapY.get(i));
+		int index = 0;
+		for (int i = 1; i <= p.length; i++) {
+			// p(p(y))=x
+			p[index] = MapY.get(MapY.get(i));
 			index++;
 		}
 		return p;
-    }
+	}
 
 	public static void main(String[] args) {
-		int[] p = { 4, 3 ,5 ,1, 2};
+		int[] p = { 4, 3, 5, 1, 2 };
 		System.out.println(permutationEquation(p));
 	}
 
